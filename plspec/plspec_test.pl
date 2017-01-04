55d5ae2b74e4899a92279a8981e60e0f7f290ff7
Philipp Koerner <P.koerner@uni-duesseldorf.de>
2017-01-04 15:34:51 +0100

    add tests

--- plspec/plspec_test.pl
+++ plspec/plspec_test.pl
@@ -1,4 +1,5 @@
 :- use_module(plspec).
+:- use_module(library(plunit)).
 
 :- plspec:spec_pre(my_member/2,[any,[any]]).
 :- plspec:spec_post(my_member/2,[any,[ground]],[ground,[ground]]).
@@ -9,3 +10,29 @@ my_member(E,[_|T]) :-
 
 foo(A,B) :-
     my_member(A,B).
+
+
+:- begin_tests(my_member_spec).
+
+test(instantiated_call, [nondet]) :-
+    my_member(a, [a, b, c]).
+
+test(partly_instantiated, [nondet]) :-
+    my_member(_, [a, b, c]).
+
+test(partly_instantiated2, [nondet]) :-
+    my_member(a, _).
+
+test(partly_instantiated3, [nondet]) :-
+    my_member(c, [a, _, c]).
+
+test(all_variables, [nondet]) :-
+    my_member(_, _).
+
+test(not_conform, [throws(_)]) :-
+    my_member([], a).
+
+:- end_tests(my_member_spec).
+
+
+
