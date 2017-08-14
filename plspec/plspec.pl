2e2d93e417be9b53595cadce181eb152aaa7354a
Philipp Koerner <p.koerner@uni-duesseldorf.de>
2017-08-14 10:21:13 +0200

    fix merge

--- plspec/plspec.pl
+++ plspec/plspec.pl
@@ -126,81 +126,78 @@ spec_predicate_recursive(tuple(X), tuple(X), and, and_invariant).
 spec_connective(and([H|T]), spec_and([H|T]), and, and_invariant).
 spec_connective(one_of(X), spec_and(X), or, or_invariant).
 
+
+pretty_print_error(fail(postcondition_violated(matched_pre(Pre), violated_post(Post), value(Val)))) :-
+    format('~n! plspec: a postcondition was violated!~n', []),
+    format('! plspec: the matched precondition was "~w"~n', [Pre]),
+    format('! plspec: however, the postcondition "~w" does not hold~n', [Post]),
+    format('! plspec: the offending value was: ~w~n', [Val]).
+pretty_print_error(fail(prespec_violated(specs(PreSpecs), values(Vals), location(Functor)))) :-
+    format('~n! plspec: no precondition was matched in ~w~n', [Functor]),
+    format('! plspec: specified preconditions were: ~w~n', [PreSpecs]),
+    format('! plspec: however, none of these is matched by: ~w~n', [Vals]).
+pretty_print_error(fail(spec_violated(spec(T), value(V), location(Location)))) :-
+    format('~n! plspec: an invariant was violated in ~w~n', [Location]),
+    format('! plspec: the spec was: ~w~n', [T]),
+    format('! plspec: however, the value was bound to: ~w~n', [V]).
+pretty_print_error(fail(spec_not_found(spec(Spec)))) :-
+    %% TODO: not all failures include a location
+    format('~n! plspec: spec "~w" was not found~n', [Spec]).
+pretty_print_error(fail(spec_not_found(spec(Spec), location(Location)))) :-
+    format('~n! plspec: a spec for ~w was not found~n', [Location]),
+    format('! plspec: spec "~w" was not found~n', [Spec]).
+pretty_print_error(X) :-
+    format('~n! plspec: plspec raised an error that is unhandled.~n', []),
+    format('! plspec: ~w.~n', [X]).
+
+pretty_print_error(fail(postcondition_violated(matched_pre(Pre), violated_post(Post), value(Val)))) :-
+    format('~n! plspec: a postcondition was violated!~n', []),
+    format('! plspec: the matched precondition was "~w"~n', [Pre]),
+    format('! plspec: however, the postcondition "~w" does not hold~n', [Post]),
+    format('! plspec: the offending value was: ~w~n', [Val]).
+pretty_print_error(fail(prespec_violated(specs(PreSpecs), values(Vals), location(Functor)))) :-
+    format('~n! plspec: no precondition was matched in ~w~n', [Functor]),
+    format('! plspec: specified preconditions were: ~w~n', [PreSpecs]),
+    format('! plspec: however, none of these is matched by: ~w~n', [Vals]).
+pretty_print_error(fail(spec_violated(spec(T), value(V), location(Location)))) :-
+    format('~n! plspec: an invariant was violated in ~w~n', [Location]),
+    format('! plspec: the spec was: ~w~n', [T]),
+    format('! plspec: however, the value was bound to: ~w~n', [V]).
+pretty_print_error(fail(spec_not_found(spec(Spec)))) :-
+    %% TODO: not all failures include a location
+    format('~n! plspec: spec "~w" was not found~n', [Spec]).
+pretty_print_error(fail(spec_not_found(spec(Spec), location(Location)))) :-
+    format('~n! plspec: a spec for ~w was not found~n', [Location]),
+    format('! plspec: spec "~w" was not found~n', [Spec]).
+pretty_print_error(X) :-
+    format('~n! plspec: plspec raised an error that is unhandled.~n', []),
+    format('! plspec: ~w.~n', [X]).
+
+pretty_print_error(fail(postcondition_violated(matched_pre(Pre), violated_post(Post), value(Val)))) :-
+    format('~n! plspec: a postcondition was violated!~n', []),
+    format('! plspec: the matched precondition was "~w"~n', [Pre]),
+    format('! plspec: however, the postcondition "~w" does not hold~n', [Post]),
+    format('! plspec: the offending value was: ~w~n', [Val]).
+pretty_print_error(fail(prespec_violated(specs(PreSpecs), values(Vals), location(Functor)))) :-
+    format('~n! plspec: no precondition was matched in ~w~n', [Functor]),
+    format('! plspec: specified preconditions were: ~w~n', [PreSpecs]),
+    format('! plspec: however, none of these is matched by: ~w~n', [Vals]).
+pretty_print_error(fail(spec_violated(spec(T), value(V), location(Location)))) :-
+    format('~n! plspec: an invariant was violated in ~w~n', [Location]),
+    format('! plspec: the spec was: ~w~n', [T]),
+    format('! plspec: however, the value was bound to: ~w~n', [V]).
+pretty_print_error(fail(spec_not_found(spec(Spec)))) :-
+    %% TODO: not all failures include a location
+    format('~n! plspec: spec "~w" was not found~n', [Spec]).
+pretty_print_error(fail(spec_not_found(spec(Spec), location(Location)))) :-
+    format('~n! plspec: a spec for ~w was not found~n', [Location]),
+    format('! plspec: spec "~w" was not found~n', [Spec]).
+pretty_print_error(X) :-
+    format('~n! plspec: plspec raised an error that is unhandled.~n', []),
+    format('! plspec: ~w.~n', [X]).
+
 :- dynamic error_handler/1.
 error_handler(plspec_default_error_handler).
-
-plspec_default_error_handler(X) :-
-    pretty_print_error(X),
-    throw(plspec_error).
-
-pretty_print_error(fail(postcondition_violated(matched_pre(Pre), violated_post(Post), value(Val)))) :-
-    format('~n! plspec: a postcondition was violated!~n', []),
-    format('! plspec: the matched precondition was "~w"~n', [Pre]),
-    format('! plspec: however, the postcondition "~w" does not hold~n', [Post]),
-    format('! plspec: the offending value was: ~w~n', [Val]).
-pretty_print_error(fail(prespec_violated(specs(PreSpecs), values(Vals), location(Functor)))) :-
-    format('~n! plspec: no precondition was matched in ~w~n', [Functor]),
-    format('! plspec: specified preconditions were: ~w~n', [PreSpecs]),
-    format('! plspec: however, none of these is matched by: ~w~n', [Vals]).
-pretty_print_error(fail(spec_violated(spec(T), value(V), location(Location)))) :-
-    format('~n! plspec: an invariant was violated in ~w~n', [Location]),
-    format('! plspec: the spec was: ~w~n', [T]),
-    format('! plspec: however, the value was bound to: ~w~n', [V]).
-pretty_print_error(fail(spec_not_found(spec(Spec)))) :-
-    %% TODO: not all failures include a location
-    format('~n! plspec: spec "~w" was not found~n', [Spec]).
-pretty_print_error(fail(spec_not_found(spec(Spec), location(Location)))) :-
-    format('~n! plspec: a spec for ~w was not found~n', [Location]),
-    format('! plspec: spec "~w" was not found~n', [Spec]).
-pretty_print_error(X) :-
-    format('~n! plspec: plspec raised an error that is unhandled.~n', []),
-    format('! plspec: ~w.~n', [X]).
-
-pretty_print_error(fail(postcondition_violated(matched_pre(Pre), violated_post(Post), value(Val)))) :-
-    format('~n! plspec: a postcondition was violated!~n', []),
-    format('! plspec: the matched precondition was "~w"~n', [Pre]),
-    format('! plspec: however, the postcondition "~w" does not hold~n', [Post]),
-    format('! plspec: the offending value was: ~w~n', [Val]).
-pretty_print_error(fail(prespec_violated(specs(PreSpecs), values(Vals), location(Functor)))) :-
-    format('~n! plspec: no precondition was matched in ~w~n', [Functor]),
-    format('! plspec: specified preconditions were: ~w~n', [PreSpecs]),
-    format('! plspec: however, none of these is matched by: ~w~n', [Vals]).
-pretty_print_error(fail(spec_violated(spec(T), value(V), location(Location)))) :-
-    format('~n! plspec: an invariant was violated in ~w~n', [Location]),
-    format('! plspec: the spec was: ~w~n', [T]),
-    format('! plspec: however, the value was bound to: ~w~n', [V]).
-pretty_print_error(fail(spec_not_found(spec(Spec)))) :-
-    %% TODO: not all failures include a location
-    format('~n! plspec: spec "~w" was not found~n', [Spec]).
-pretty_print_error(fail(spec_not_found(spec(Spec), location(Location)))) :-
-    format('~n! plspec: a spec for ~w was not found~n', [Location]),
-    format('! plspec: spec "~w" was not found~n', [Spec]).
-pretty_print_error(X) :-
-    format('~n! plspec: plspec raised an error that is unhandled.~n', []),
-    format('! plspec: ~w.~n', [X]).
-
-pretty_print_error(fail(postcondition_violated(matched_pre(Pre), violated_post(Post), value(Val)))) :-
-    format('~n! plspec: a postcondition was violated!~n', []),
-    format('! plspec: the matched precondition was "~w"~n', [Pre]),
-    format('! plspec: however, the postcondition "~w" does not hold~n', [Post]),
-    format('! plspec: the offending value was: ~w~n', [Val]).
-pretty_print_error(fail(prespec_violated(specs(PreSpecs), values(Vals), location(Functor)))) :-
-    format('~n! plspec: no precondition was matched in ~w~n', [Functor]),
-    format('! plspec: specified preconditions were: ~w~n', [PreSpecs]),
-    format('! plspec: however, none of these is matched by: ~w~n', [Vals]).
-pretty_print_error(fail(spec_violated(spec(T), value(V), location(Location)))) :-
-    format('~n! plspec: an invariant was violated in ~w~n', [Location]),
-    format('! plspec: the spec was: ~w~n', [T]),
-    format('! plspec: however, the value was bound to: ~w~n', [V]).
-pretty_print_error(fail(spec_not_found(spec(Spec)))) :-
-    %% TODO: not all failures include a location
-    format('~n! plspec: spec "~w" was not found~n', [Spec]).
-pretty_print_error(fail(spec_not_found(spec(Spec), location(Location)))) :-
-    format('~n! plspec: a spec for ~w was not found~n', [Location]),
-    format('! plspec: spec "~w" was not found~n', [Spec]).
-pretty_print_error(X) :-
-    format('~n! plspec: plspec raised an error that is unhandled.~n', []),
-    format('! plspec: ~w.~n', [X]).
 
 :- public plspec_default_error_handler/1.
 plspec_default_error_handler(X) :-
