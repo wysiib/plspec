dbf605be1ba6e0b69fdfaf5a304573657587ac61
Isabel Wingen <isabel.wingen@hhu.de>
2018-05-06 13:47:41 +0200

    Add list(spec) as a recursive predicate

--- plspec/plspec2.pl
+++ plspec/plspec2.pl
@@ -1,4 +1,5 @@
-:- use_module(library(lists), [maplist/2, maplist/3, maplist/4, is_list/1]).
+:- module(plspec2, [spec_predicate/2, spec_exists/1, validate/2]).
+
 :- use_module(library(terms), [variant/2]).
 
 
@@ -14,8 +15,11 @@ spec_predicate(ground, ground).
 spec_predicate(nonvar, nonvar).
 spec_predicate(any, true).
 
+spec_predicate_recursive(list(X), list(X), and, and_invariant).
+
 %When does a predicate exists:
 spec_exists(X) :- spec_predicate(X, _).
+spec_exists(X) :- spec_predicate_recursive(X, _, _, _).
 
 %TODO: why is this needed
 :- public true/1.
@@ -49,3 +53,33 @@ evaluate_spec_match_aux(Spec, Val, Res) :-
      -> Res = true
      ; Res = fail(spec_not_matched(spec(Spec), value(Val)))),
     (copy_term(Val, Valii), variant(Valii, Vali) -> true ; format('plspec: implementation of spec ~w binds variables but should not~n', [Predicate])).
+% a recursive spec
+evaluate_spec_match_aux(Spec, Val, Res) :-
+    spec_predicate_recursive(Spec, Predicate, MergePred, _MergePredInvariant),
+    copy_term(Val, Vali),
+    (call(Predicate, Val, NewSpecs, NewVals)
+     -> call(MergePred, NewSpecs, NewVals, Res)
+     ; Res = fail(spec_not_matched(spec(Spec), value(Val)))),
+    (copy_term(Val, Valii), variant(Valii, Vali) -> true ; format('plspec: implementation of spec ~w binds variables but should not~n', [Predicate])).
+
+:- public and/3.
+and([], [], true).
+and([S|Specs], [V|Vals], Res) :-
+    evaluate_spec_match(S, V, X),
+    (X == true
+     -> and(Specs, Vals, Res)
+     ; Res = fail(spec_not_matched(spec(S), value(V)))).
+
+list(Spec, Val, NewSpecs, NewVals) :-
+    nonvar(Val), list1(Val, Spec, NewSpecs, NewVals).
+%% list1 only ensures that the value is a list.
+%% The type of its members is checked later on in a seperate step.
+%% list1 will return a spec for each member.
+%% If a tail is a variable, the bound value should be
+%% of the same type as the list itself.
+list1(L, Spec, [Spec|ST], [H|VT]) :-
+    nonvar(L), L = [H|T], !,
+    list1(T, Spec, ST, VT).
+list1(L, _, [], []) :-
+    nonvar(L), L = [], !.
+list1(Var, Spec, [list(Spec)], [Var]) :- var(Var).
