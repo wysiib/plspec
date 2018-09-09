e7c8e78a0791b2c0dda4666c1639e00b271f3630
Isabel Wingen <isabel.wingen@hhu.de>
2018-09-09 15:12:54 +0200

    WIP Pre Spec Prediction
    Collect spec information about variables in a associate state. At the moment
    it only prints the collected information, analysis is still missing.

--- plspec/static_analyzer.pl
+++ plspec/static_analyzer.pl
@@ -1,25 +1,84 @@
-process_source(Src) :-
+:-module(static_analyzer,[analyze_source/1]).
+
+analyze_source(Src) :-
+    process_source(Src,process_directs),
+    process_source(Src,abs_int).
+
+process_source(Src,Goal) :-
     prolog_canonical_source(Src,CanSrc),
     prolog_open_source(CanSrc,In),
-    call_cleanup(process(In), prolog_close_source(In)).
+    Process =.. [Goal,In],
+    call_cleanup(Process,prolog_close_source(In)).
 
 
-
-process(Stream) :-
+process_directs(Stream) :-
     prolog_read_source_term(Stream,Term,Expanded,[]),!,
     (Term = end_of_file
      ->
          true
      ;
-         interpret(Expanded),
-         process(Stream)).
+         execute_directs(Expanded),!,
+         process_directs(Stream)).
 
-interpret(':-'(A)) :-
-    write(A),nl,
-    call(A), !.
-interpret(':-'(A,B)) :-
-    write(A),write(" :- "),nl, !,
-    what_is_B(B),nl.
+execute_directs(':-'(A)) :-
+    !,call(A).
+execute_directs(_) :- !.
+
+abs_int(Stream) :-
+    prolog_read_source_term(Stream,Term,Expanded,[]),!,
+    (Term = end_of_file
+     ->
+         true
+     ;
+         empty_assoc(Env),
+         analyze_term(Expanded,Env),!,
+         abs_int(Stream)).
+
+
+find_specs_to_goal(Goal,Specs) :-
+    name_with_module(Goal,FullName),
+    findall(Spec,asserted_spec_pre(FullName,Spec,_),Specs).
+
+write_condition(Goal,EnvIn,EnvOut) :-
+    Goal =.. [_|Args],
+    create_empty_value_if_not_exists(Args,EnvIn,EnvWorking),
+    find_specs_to_goal(Goal,Specs),
+    get_assoc(Args,EnvWorking,L,Env2,[one_of(Specs)|L]),
+    assoc_single_values(Args,Specs,Env2,EnvOut).
+
+
+create_empty_value_if_not_exists(Key,Assoc,Assoc) :-
+    get_assoc(Key,Assoc,_), !.
+create_empty_value_if_not_exists(Key,Assoc,NewAssoc) :-
+    put_assoc(Key,Assoc,[],NewAssoc).
+
+assoc_single_values([],_,Env,Env) :- !.
+assoc_single_values([H|Args],Specs,EnvIn,EnvOut) :-
+    maplist(nth0(0),Specs,SpecsForH),
+    create_empty_value_if_not_exists(H,EnvIn,EnvWorking),
+    get_assoc(H,EnvWorking,L,EnvWorking2,[one_of(SpecsForH)|L]),
+    assoc_single_values(Args,Specs,EnvWorking2,EnvOut).
+
+
+analyze_term(':-'(_),_StateIn) :- !.
+analyze_term(':-'(A,B),StateIn) :-
+    write_condition(A,StateIn,State2),
+    analyze_body(B,State2,StateOut),
+    assoc_to_list(StateOut,Res),
+    write(A), write(": "), write(Res), nl, nl.
+
+analyze_body((B,C),In,Out) :- !,
+    write_condition(B,In,Between),
+    analyze_body(C,Between,Out).
+analyze_body((C),In,Out) :-
+    write_condition(C,In,Out).
+
+
+name_with_module(Compound,FullName) :-
+    Compound =.. [Name|Args],
+    length(Args,Arity),
+    prolog_load_context(module,Module),
+    FullName = Module:Name/Arity.
 
 do_expand(
         ':-'(spec_pre(Predicate/Arity, Spec)),
